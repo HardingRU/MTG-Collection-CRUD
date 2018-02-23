97c1d39c807a28b192028dd81af38984011d1661
Chris Harding <christopher.harding@gmx.com>
2018-02-23 15:31:09 -0500

    added dbconfig, seeds, schema, created DB and seeded with starting data

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+\c mtg;
+
+DROP TABLE IF EXISTS sets;
+DROP TABLE IF EXISTS cards;
+DROP TABLE IF EXISTS decks;
+DROP TABLE IF EXISTS cards_decks;
+
+CREATE TABLE sets (
+  id SERIAL PRIMARY KEY,
+  set_name VARCHAR(255)
+);
+
+CREATE TABLE cards (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  mana_cost VARCHAR(255),
+  color TEXT[],
+  rarity VARCHAR(255),
+  type VARCHAR(255),
+  card_text VARCHAR(255),
+  attack VARCHAR(255),
+  defense VARCHAR(255),
+  img_url VARCHAR(255),
+  num_cards INT,
+  set_id INTEGER REFERENCES sets
+);
+
+CREATE TABLE decks (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE cards_decks (
+  deck_id SERIAL PRIMARY KEY,
+  card_id INT,
+  num_cards INT
+);
---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+\c mtg;
+
+INSERT INTO sets (set_name) VALUES ('Khans of Tarkhir');
+INSERT INTO cards (name, mana_cost, color, rarity, type, card_text, attack, defense, img_url, num_cards, set_id) VALUES
+  (
+    'Narset, Enlightened Master',
+    '{3}{U}{R}{W}',
+    ARRAY['White', 'Blue', 'Red'],
+    'Mythic Rare',
+    'Legendary Creature - Human Monk',
+    'First strike, hexproof. Whenever Narset, Enlightened Master attacks, exile the top four cards of your library. Until end of turn, you may cast noncreature cards exiled with Narset this turn without paying their mana costs.',
+    '3',
+    '2',
+    'http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=386616&type=card',
+    '4',
+    '1'
+  );
