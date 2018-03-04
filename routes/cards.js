4ff5f90ba4df92f21c56b76b8d5842f7297a5206
Chris Harding <christopher.harding@gmx.com>
2018-03-04 18:09:56 -0500

    added comments to routers

--- routes/cards.js
+++ routes/cards.js
@@ -3,23 +3,23 @@
 const cardsRouter = express.Router();
 const cardsController = require('../controllers/cardsController');
 const viewsController = require('../controllers/viewsController');
-
+// get all cards
 cardsRouter.get('/', cardsController.index, viewsController.showCards, viewsController.show404);
+//create new card
 cardsRouter.post('/', cardsController.create, viewsController.cardCreate);
-
+//used as part of an internal API call to get all card data and pass to editDeck.js for rending cards on edit deck page 
 cardsRouter.get('/data/', cardsController.index, viewsController.jsonCards, viewsController.show404);
-//cardsRouter.get('/new', setsController.index, cardsController.makeBlankCard, viewsController.addCardForm);
-
+//search for card using 3rd party API
 cardsRouter.get('/search', viewsController.search, viewsController.show404);
-
+//open pack functionality
 cardsRouter.get('/pack', viewsController.openPack, viewsController.show404);
-
+//single card page functionality
 cardsRouter.get('/:id', cardsController.getCard, viewsController.showCard, viewsController.show404);
+//update single card
 cardsRouter.put('/:id', cardsController.update, viewsController.updateCard);
+//delete single card
 cardsRouter.delete('/:id', cardsController.destroyCard, viewsController.deleteCard);
 
 cardsRouter.use('*', viewsController.show404)
-
-//cardsRouter.get('/:id/edit', cardsController.getCard, viewsController.editCardForm)
 
 module.exports = cardsRouter;
--- routes/decks.js
+++ routes/decks.js
@@ -5,22 +5,25 @@ const decksRouter = express.Router();
 const decksController = require('../controllers/decksController');
 const viewsController = require('../controllers/viewsController');
 const cardsController = require('../controllers/cardsController');
-
+//get all decks and render
 decksRouter.get('/', decksController.index, viewsController.showDecks, viewsController.show404);
+//create a new deck
 decksRouter.post('/', decksController.create, viewsController.deckCreate);
-
+//used as internal API call to add cards to a deck from the collection
 decksRouter.post('/addToDeck', decksController.addToDeck, viewsController.deckEditRedirect);
+//used as internal service to get user changes to deck and update accordingly
 decksRouter.put('/saveDeck', decksController.saveDeck)
-
+//front-end functionality to create new deck via user input
 decksRouter.get('/new', decksController.makeBlankDeck, viewsController.addDeckForm, viewsController.show404);
-
+//functionality to remove a single card from deck, used when user changes number value to 0 for a card in deck
 decksRouter.delete('/removeCard', decksController.removeCard)
-
+//get single deck and render
 decksRouter.get('/:id', decksController.getDeck, viewsController.showDeck, viewsController.show404);
-
+//update a single deck back-end
 decksRouter.put('/:id', decksController.update, viewsController.updateDeck);
+//delete a deck
 decksRouter.delete('/:id', decksController.destroyDeck, viewsController.deleteDeck, viewsController.show404);
-
+//edit a deck functionality
 decksRouter.get('/:id/edit', cardsController.index, decksController.getDeck, viewsController.editDeckForm, viewsController.show404)
 
 decksRouter.use('*', viewsController.show404)
