a28394cffa4096a83ef3205eafac8d79f476438d
Chris Harding <christopher.harding@gmx.com>
2018-03-05 08:23:59 -0500

    typo fixed

--- public/src/editDeck.js
+++ public/src/editDeck.js
@@ -87,7 +87,7 @@ $(()=>{
     data.forEach(card => {
       const cardLink = "/cards/" + card.id;
       $('#editTable').append(`<tr id='editItem${card.id}'>`)
-      $(`#editItem${card.id}`).append(`<td class=deckDiv id=deckDiv><a href=` + cardLink + ` class="preview">` + card.name + `<img src=` +card.img_url + ` class="hide-image" /></a></td>`);
+      $(`#editItem${card.id}`).append(`<td class=deckDiv id=deckDiv` + card.id + `><a href=` + cardLink + ` class="preview">` + card.name + `<img src=` +card.img_url + ` class="hide-image" /></a></td>`);
       $(`#editItem${card.id}`).append("<td> " + card.colors + " </td>");
       $(`#editItem${card.id}`).append("<td> " + card.mana_cost + " </td>");
       $(`#editItem${card.id}`).append("<td> " + card.type + " </td>");
